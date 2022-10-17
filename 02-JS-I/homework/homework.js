ba98a2a6bb76a9408d03d65650264c484503e1f1
Tomysola78 <tomasjsola@gmail.com>
2022-10-17 12:40:11 -0300

    TareaJS1

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = "Hola";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = "prueba";
+const nuevoNum = 78 ;
 
 // Crea una variable booleana:
 const nuevoBool = null;
